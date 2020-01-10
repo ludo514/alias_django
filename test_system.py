9c6679eb23220e8693bf8dc5b72d0dc09d032a85
ludo <ludovict40@gmail.com>
2020-01-10 21:52:46 +0100

    adding try except

--- test_system.py
+++ test_system.py
@@ -1,9 +1,12 @@
 import os
-import subprocess
 from sys import argv
 
-os.system("mkdir {}".format(argv[1]))
-os.chdir("{}".format(argv[1]))
-os.system("virtualenv env")
-os.system("django-admin startproject base")
-os.system('cmd /k "env\\Scripts\\activate.bat && pip install Django && cd base && python manage.py startapp {}"'.format(argv[1]))
+
+try:
+    os.system("mkdir {}".format(argv[1]))
+    os.chdir("{}".format(argv[1]))
+    os.system("virtualenv env")
+    os.system("django-admin startproject base")
+    os.system('cmd /k "env\\Scripts\\activate.bat && pip install Django && cd base && python manage.py startapp {}"'.format(argv[1]))
+except IndexError:
+    print("Erreur entrer")
