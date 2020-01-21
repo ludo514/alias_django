5037fab152bb57469ff5cec3313fa640934541ce
ludo <ludovict40@gmail.com>
2020-01-21 22:42:52 +0100

    modif

--- test_system.py
+++ test_system.py
@@ -7,6 +7,6 @@ try:
     os.chdir("{}".format(argv[1]))
     os.system("virtualenv env")
     os.system("django-admin startproject base")
-    os.system('cmd /k "env\\Scripts\\activate.bat && pip install Django && pip install django-extensions &&cd base && python manage.py startapp {}"'.format(argv[1]))
+    os.system('cmd /k "env\\Scripts\\activate.bat && pip install Django && pip install django-extensions && cd base && python manage.py startapp {}"'.format(argv[1]))
 except IndexError:
     print("Erreur entrer")
